dc5967ebd6ba30d64ce946261e0293a3298acf81
iowar <oguzhankaymak@yandex.com>
2018-01-27 23:13:21 +0100

    change mutex

--- logger.go
+++ logger.go
@@ -7,16 +7,16 @@ import (
 
 type Logger struct {
 	isOpen bool
-	mu     *sync.Mutex
+	Lock   *sync.Mutex
 }
 
 func (l *Logger) LogRoutine(bus <-chan string) {
 	if l.isOpen {
 		for {
 			message := <-bus
-			l.mu.Lock()
+			l.Lock.Lock()
 			fmt.Println(message)
-			l.mu.Unlock()
+			l.Lock.Unlock()
 		}
 	}
 }
